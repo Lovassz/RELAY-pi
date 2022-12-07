b408ca8432a6bf3ee5348e7f940755f8a16cb1f3
Lovi <olovio@gmail.com>
2022-12-07 19:52:47 +0100

    Estimated next call log

--- endpoint_switch.php
+++ endpoint_switch.php
@@ -2,12 +2,17 @@
 
 if (!isset($_GET['interval']) || empty($_GET['interval'])) exit;
 
-file_put_contents('logs/'. date('Y-m-d') .'.csv', implode(',', 
+$currentCallEstimated = intval(@file_get_contents('data/switch_esimated_next_call.log') ?? 0);
+@file_put_contents('data/switch_esimated_next_call.log', time() + intval($_GET['interval'] ?? '') / 1000000);
+@file_put_contents('logs/'. date('Y-m-d') .'.csv', implode(',', 
     [
         date('H:i:s'),
         time(),
         intval($_GET['interval'] ?? ''),
+        (time() - $currentCallEstimated),
     ]
 ) ."\r\n", FILE_APPEND);
 
 echo 'ok';
+
+//http://192.168.0.17/cm?user=admin&password=stupid&cmnd=Power%20On
