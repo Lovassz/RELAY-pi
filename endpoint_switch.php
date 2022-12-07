01590011be2aedb9e4ac8c5531f3ce41576390e1
Lovi <olovio@gmail.com>
2022-12-07 14:33:24 +0100

    #3

--- endpoint_switch.php
+++ endpoint_switch.php
@@ -3,3 +3,5 @@
 if (!isset($_GET['interval']) || empty($_GET['interval'])) exit;
 
 file_put_contents('logs/'. date('Y-m-d') .'.csv', date('H:i:s,') . json_encode($_GET['interval'] ?? '') ."\r\n", FILE_APPEND);
+
+echo 'ok';
