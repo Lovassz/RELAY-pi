1f5dfe495c4ddee3d0f6007ba6c625737ad19d98
Lovi <olovio@gmail.com>
2022-12-15 18:05:57 +0100

    IOS Widget v2.1

--- iOS_widget.js
+++ iOS_widget.js
@@ -1,37 +1,69 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: magic;
-const dataUrl = "http://Lovi:@192.168.1.100/RELAY/RELAY-pi/endpoint_status.php";
+const dataUrl = "http://Lovi:@192.168.0.18/endpoint_status.php";
 
 let widget = await createWidget();
 Script.setWidget(widget);
-widget.presentMedium();
+widget.presentSmall();
 Script.complete();
 
 async function createWidget() {
+
     const widget = new ListWidget();
 
-    const data = await new Request(dataUrl).loadJSON();
+    let data = {
+        "serverStatus": "ERROR",
+        "switchStatus": "ERROR",
+        "relayStatus" : "ERROR"
+    };
 
-    let titleRow = widget.addText(`Losers ${data.timestamp}`);
-    titleRow.font = Font.boldSystemFont(15);
-    titleRow.textColor = Color.white();
-    widget.addSpacer(5);
+    const req = new Request(dataUrl);
+    req.allowInsecureRequest = true;
+    try {
 
-    for (i = 0; i < 7; i++) {
-        const looser = data.loosers[i];
-        let row = leftColumn.addText(`
-            ${looser.ticker} 
-            ${looser.change}
-        `);
-        row.font = Font.semiboldSystemFont(14);
+        data = await req.loadJSON();
+
+    } catch(err) {}
+
+    if (data.serverStatus + data.switchStatus + data.relayStatus !== 'OKOKOK') {
+        let noti      = new Notification()
+        noti.title    = "OUTGE!!!";
+        noti.body     = "A RELAY egyik komponense nem válaszol!";
+        noti.schedule();
     }
 
-    let gradient = new LinearGradient()
+    let titleRow = widget.addText(`Ping status`);
+    titleRow.font = Font.boldSystemFont(15);
+    titleRow.textColor = Color.white();
+
+    // Time
+    let timeRow = widget.addText( ('0'+(new Date()).getHours()).substr(-2) +':'+ ('0'+(new Date()).getMinutes()).substr(-2) );
+    timeRow.font = Font.boldSystemFont(10);
+    timeRow.textColor = Color.white();
+
+    widget.addSpacer(10);
+
     
-    gradient.colors = [new Color("3a8cc1"), new Color("00A9D6")];
-    gradient.locations =  [0, 1];
+    // Server
+    let serwerRow = widget.addText(`Server ${data.serverStatus}`);
+    serwerRow.font = Font.boldSystemFont(15);
+    serwerRow.textColor = (data.switchStatus === 'OK') ? Color.green() : Color.red();
+
     
-    widget.backgroundGradient = gradient
+    // Switch
+    let switchRow = widget.addText(`Switch ${data.switchStatus}`);
+    switchRow.font = Font.boldSystemFont(15);
+    switchRow.textColor = (data.switchStatus === 'OK') ? Color.green() : Color.red();
+
+    
+    // Relay
+    let relayRow = widget.addText(`Relay ${data.relayStatus}`);
+    relayRow.font = Font.boldSystemFont(15);
+    relayRow.textColor = (data.relayStatus === 'OK') ? Color.green() : Color.red();
+
+    let bgColor = new Color("#191d28", 1);
+    
+    widget.backgroundColor = bgColor
     return widget;
-}
+};
